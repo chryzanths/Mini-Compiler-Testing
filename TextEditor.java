fe3ef6e8aee24a7af13db555a04576a1c0a8dc05
chryzle <104879763+chryzanths@users.noreply.github.com>
2023-12-16 15:42:49 +0800

    Update TextEditor.java

--- TextEditor.java
+++ TextEditor.java
@@ -16,6 +16,7 @@ public class TextEditor extends JFrame implements ActionListener {
     JButton syntaxAnalyzer;
     JButton semanticsAnalyzer;
     TextEditor(){
+        // text editor pane
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setTitle("Add source code");
         this.setSize(600, 400);
@@ -26,8 +27,9 @@ public class TextEditor extends JFrame implements ActionListener {
         sourceCode = new JTextArea();
         sourceCode.setLineWrap(true);
         sourceCode.setWrapStyleWord(true);
-        sourceCode.setFont(new Font("Monospaced", Font.CENTER_BASELINE, 15));
+        sourceCode.setFont(new Font("Monospaced", Font.CENTER_BASELINE, 15)); //
 
+        // pang scroll kase baka di kumasya sa editor pag madaming lines of code
         scrollPane = new JScrollPane(sourceCode);
         scrollPane.setPreferredSize(new Dimension(400,200));
         scrollPane.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
@@ -80,23 +82,27 @@ public class TextEditor extends JFrame implements ActionListener {
         if(e.getSource()==fileOpener){
             JFileChooser fileChooser = new JFileChooser();
             fileChooser.setCurrentDirectory(new File("."));
+            // txt file lang tinatanggap para di complicated
             FileNameExtensionFilter filter = new FileNameExtensionFilter("txt files", "txt");
             fileChooser.setFileFilter(filter);
 
+            // pang open nung pop up window pag pipili ng files
             int response = fileChooser.showOpenDialog(null);
 
             if(response == JFileChooser.APPROVE_OPTION){
+                // dito i-scan contents ng file
                 File file = new File(fileChooser.getSelectedFile().getAbsolutePath());
                 Scanner fileScanner = null;
 
                 try {
                     fileScanner = new Scanner(file);
-                    if(file.isFile()){
+                    if(file.isFile()){ // check if yung file na in-upload is valid
                         while(fileScanner.hasNextLine()){
+                            // ewan pero dito na papasok yung seperation ng strings sa maeextract sa file
                             lexemes = Testing.separateCode(String.valueOf(file));
                             String codeLine = fileScanner.nextLine()+"\n";
-                            sourceCode.append(codeLine);
-
+                            sourceCode.append(codeLine); // this means na kung ano yung laman nung file na naupload, magrereflect sa text box
+                            
                             if(e.getSource()==lexicalAnalyzer){
 
                                 tokens = Testing.lexicalAnalysis(lexemes);
