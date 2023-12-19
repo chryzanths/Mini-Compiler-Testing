75c734873c13e9bc12c43ac602538f6cb0608d5a
Jhan Ellen Atole <ellenatole@gmail.com>
2023-12-19 14:01:17 +0800

    Add files via upload

--- src/Compiler.java
+++ src/Compiler.java
@@ -12,9 +12,7 @@ import java.util.regex.Pattern;
 public class Compiler {
 
     public static List<List<String>> linesOfCode = new ArrayList<>();
-    public static List<List<String>> linesOFTokens = new ArrayList<>();
-    public static List<String> lexemes = new ArrayList<>();
-    public static List<String> tokens = new ArrayList<>();
+    public static List<List<String>> linesOfTokens = new ArrayList<>();
 
     public static String semanticAnalysis() {
 
@@ -23,7 +21,7 @@ public class Compiler {
 
         for (int i = 0; i<linesOfCode.size(); i++){
             List<String> codeLine = linesOfCode.get(i);
-            List<String> tokenLine = linesOFTokens.get(i);
+            List<String> tokenLine = linesOfTokens.get(i);
             String dataType = codeLine.get(tokenLine.indexOf("<data_type>"));
             String value = codeLine.get(tokenLine.indexOf("<value>"));
 
@@ -61,12 +59,12 @@ public class Compiler {
         //<data_type> <identifier> <assignment_operator> <value> <delimiter>
         String[] syntax = {"<data_type>", "<identifier>", "<assignment_operator>", "<value>", "<delimiter>"};
 
-        for (List<String> linesOFToken : linesOFTokens) {
+        for (List<String> lineOfTokens : linesOfTokens) {
 
-            if (linesOFToken.size() == 5) {
+            if (lineOfTokens.size() == 5) {
                 for (int j = 0; j < 5; j++) {
 
-                    boolean match = Objects.equals(linesOFToken.get(j), syntax[j]);
+                    boolean match = Objects.equals(lineOfTokens.get(j), syntax[j]);
 
                     if (!match) {
                         isValid = false;
@@ -74,6 +72,8 @@ public class Compiler {
                     }
 
                 }
+            } else {
+                isValid = false;
             }
 
         }
@@ -87,7 +87,7 @@ public class Compiler {
 
     public static void lotChecker() {
 
-        for (List<String> tokenLine : linesOFTokens) {
+        for (List<String> tokenLine : linesOfTokens) {
             for (String token : tokenLine) {
                 System.out.print(token + " ");
             }
@@ -98,76 +98,98 @@ public class Compiler {
 
     public static String lexicalAnalysis(){
 
-            String[] regEx = {"int|double|float|char|String", "=", ";", "(^-?\\d+$)|(^\".*\"$)|(^'[a-zA-Z]'$)|(^-?\\d*\\.?\\d+\\.?$)|(^-?\\d*\\.?\\d+\\.?f$)","([a-zA-Z_$][a-zA-Z0-9_$]*)"};
-            String[] tokenName = {"data_type", "assignment_operator", "delimiter", "value", "identifier"};
+        String[] regEx = {"int|double|float|char|String", "=", ";", "(^-?\\d+$)|(^\".*\"$)|(^'[a-zA-Z]'$)|(^-?\\d*\\.?\\d+\\.?$)|(^-?\\d*\\.?\\d+\\.?f$)","(^[a-zA-Z_$][a-zA-Z0-9_$]*$)"};
+        String[] tokenName = {"data_type", "assignment_operator", "delimiter", "value", "identifier"};
 
-            boolean isValid = true;
+        boolean isValid = true;
 
-            for (List<String> codeLine : linesOfCode) {
+        for (List<String> codeLine : linesOfCode) {
 
-                for (String lexeme : codeLine){
-                    boolean isfound = false;
-                    boolean isDataType = false;
+            List<String> tokenLine = new ArrayList<>();
 
-                    for(int i=0; i<5; i++) {
+            for (String lexeme : codeLine){
+                boolean isfound = false;
+                boolean isDataType = false;
 
-                        Pattern pattern = Pattern.compile(regEx[i]);
-                        Matcher matcher = pattern.matcher(lexeme);
-                        boolean found = matcher.find();
+                for(int i=0; i<5; i++) {
 
-                        if (found){
+                    Pattern pattern = Pattern.compile(regEx[i]);
+                    Matcher matcher = pattern.matcher(lexeme);
+                    boolean found = matcher.find();
 
-                            isfound = true;
+                    if (found){
 
-                            if (i == 0) {
-                                isDataType = true;
-                            }
+                        isfound = true;
 
-                            if (i<4){
-                                tokens.add("<" + tokenName[i] + ">");
-                            } else {
-                                if (!isDataType){
-                                    tokens.add("<" + tokenName[i] + ">");
+                        if (i == 0) {
+                            isDataType = true;
+                        }
 
-                                }
+                        if (i<4){
+                            tokenLine.add("<" + tokenName[i] + ">");
+                        } else {
+                            if (!isDataType){
+                                tokenLine.add("<" + tokenName[i] + ">");
                             }
                         }
                     }
-
-                    if (!isfound) {
-                        isValid = false;
-                        //validate = "There is an invalid lexeme, cannot be put into token";
-                        //return validate;
-                        System.out.println("Invalid lexeme: " + lexeme);
-                    }
                 }
 
-                linesOFTokens.add(tokens);
+                if (!isfound) {
+                    isValid = false;
+                    System.out.println("Invalid lexeme: " + lexeme);
+                }
 
             }
-
-            lotChecker();
-
-            if (isValid) {
-                return "valid";
-                // For checking
-                //System.out.println(tokens);
-                //validate = "The code passed Lexical Analysis";
-                //return validate;
-            } else {
-                return "invalid";
-            }
-
+            linesOfTokens.add(tokenLine);
         }
+
+        lotChecker();
+
+        if (isValid) {
+            return "valid";
+        } else {
+            return "invalid";
+        }
+
+    }
 
     public static void locChecker() {
 
         for (List<String> codeLine : linesOfCode) {
             for (String lexeme : codeLine) {
-                System.out.print(lexeme + " ");
+                System.out.print("<" + lexeme  + "> ");
             }
             System.out.println();
         }
+
+    }
+
+    public static List<String> separateCode(String code) {
+
+        List<String> lexemes = new ArrayList<>();
+
+        Pattern pattern = Pattern.compile("(=)|(;)|(\".*\")|('.*')|([^\\s=;]+)");
+
+        Matcher matcher = pattern.matcher(code);
+
+        while (matcher.find()) {
+
+            String lexeme = matcher.group();
+
+            if (!lexeme.matches("\\s*")) {
+
+                lexemes.add(lexeme);
+
+            }
+
+        }
+
+        // For checking
+
+        //System.out.println(lexemes);
+
+        return lexemes;
 
     }
 
@@ -192,9 +214,8 @@ public class Compiler {
 
                     while (fileScanner.hasNextLine()) {
                         String codeLine = fileScanner.nextLine();
-                        System.out.println(codeLine);
                         output.append(codeLine).append("\n");
-                        lexemes = Compiler.separateCode(codeLine);
+                        List<String> lexemes = separateCode(codeLine);
                         linesOfCode.add(lexemes);
                     }
 
@@ -210,34 +231,5 @@ public class Compiler {
         return output.toString();
 
     }
-
-    public static List<String> separateCode(String code) {
-
-            List<String> lexemes = new ArrayList<>();
-
-            Pattern pattern = Pattern.compile("(=)|(;)|(\".*\")|('.*')|([^\\s=;]+)");
-
-            Matcher matcher = pattern.matcher(code);
-
-            while (matcher.find()) {
-
-                String lexeme = matcher.group();
-
-                if (!lexeme.matches("\\s*")) {
-
-                    lexemes.add(lexeme);
-
-                }
-
-            }
-
-            // For checking
-
-            //System.out.println(lexemes);
-
-            return lexemes;
-
-        }
-
 
 }
